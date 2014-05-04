ea77a145cafa05a4d6426dd0ff846074a3a6e54b
Colin Jones <colinsjones11@gmail.com>
2014-05-04 12:19:46 -0500

    Form Added - Done!
    Changed to "location" instead of "address" just seems to fit better for
    the inputs we can accept from users.

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -3,12 +3,25 @@ require 'json'
 
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 42.0538387
-    @longitude = -87.67721
+
+   if params[:location].nil?
+      @location = "Northwestern University"
+    else
+      @location = params["location"]
+    end
+
+    @url_safe_location = URI.encode(@location)
+
+    location_url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_location}&sensor=true"
+    raw_data = open(location_url).read
+    parsed_data = JSON.parse(raw_data)
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
     api_key = "0832bbed96c0963fa272685eab1057a5"
 
-    url = "https://api.forecast.io/forecast/#{api_key}/#{@latitude},#{@longitude}"
-    raw_data = open(url).read
+    weather_url = "https://api.forecast.io/forecast/#{api_key}/#{@latitude},#{@longitude}"
+    raw_data = open(weather_url).read
     parsed_data = JSON.parse(raw_data)
     @temperature = parsed_data["currently"]["temperature"]
     @minutely_summary = parsed_data["minutely"]["summary"]
